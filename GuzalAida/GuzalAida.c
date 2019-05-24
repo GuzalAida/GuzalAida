6a17e62b8c308fddded598f9b0f2e6238df7d9be
ka1i <Mardan_66@outlook.com>
2019-05-24 11:26:02 +0800

    update

--- GuzalAida/GuzalAida.c
+++ GuzalAida/GuzalAida.c
@@ -1,3 +1,12 @@
+/*
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+*/
+//GuzalAida.c  Copyright (C) 2019  Mardan
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
