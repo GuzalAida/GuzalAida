c7e1e67748fc1607f6f6a64b829c8f97253b7e42
ka1i <Mardan_66@outlook.com>
2019-05-26 13:14:23 +0800

    update

--- daemon/guzald.c
+++ daemon/guzald.c
@@ -40,7 +40,7 @@ int main(int argc, char const *argv[])
 	closelog();
 	while(1){
 		if (guzal_id == 0){
-			status = execlp("ls",NULL);
+			status = execlp("/opt/GuzalAida/bin/GuzalAida",NULL);
 		}else{
 			wait(&status);
 			syslog(LOG_INFO,"程序状态:%d",status);
